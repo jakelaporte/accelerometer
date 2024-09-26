981683ed932d80154277105aca85df4ed6262bca
Jake LaPorte <49242583+jakelaporte@users.noreply.github.com>
2024-09-26 18:26:50 -0500

    Update steps_model_v02.py

--- steps_model_v02.py
+++ steps_model_v02.py
@@ -35,10 +35,11 @@ class AccelerationData(object):
             self.df['time']=data.iloc[1:,0].copy()
             self.df[['x','y','z']] = data.iloc[1:,1:3].copy()
         self.time = np.arange(0,len(self.df)*self.delta_t,self.delta_t)
-        self.df['t']=self.time
+        
         st.write(len(self.df),len(self.time))
         st.write(self.df)
         st.write(self.time)
+        self.df['t']=self.time
         #example_duration - the time (in seconds) used to calculate statistics
         #smaller is better, but increases the amount of storage and time needed to calculate
         self.example_duration = 10
